760cda276127a206db3c15e843d349a9dade7182
GitHub Action <action@github.com>
2021-03-26 06:27:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/siriusintardis/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/siriusintardis/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.siriusintardis.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.siriusintardis.ideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/siriusintardis/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/siriusintardis/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.siriusintardis.ideaplugin.services
 
+import com.github.siriusintardis.ideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
